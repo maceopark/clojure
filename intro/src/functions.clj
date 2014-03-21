33268171bf6fe42b2660f6aa70617ac92e7b9d51
Nochul Park <maceo.park@gmail.com>
2014-03-21 01:16:22 -0700

    modify functions

--- intro/src/functions.clj
+++ intro/src/functions.clj
@@ -11,3 +11,14 @@
 (messenger)
 
 (messenger "Hello class!")
+
+(defn fac
+  "Returns the factorian of n, which must be a positive integer"
+  [n]
+  (if (= n 1)
+    1
+    (* n (fac(- n 1)))))
+
+(fac 5)
+
+(* 3 (+ 1 2 3) 4 5 6)
